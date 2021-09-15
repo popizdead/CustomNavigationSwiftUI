56d82ddcca72f53d95a7284e769eddb5bc5191d8
popizdead <godissk8@gmail.com>
2021-09-16 02:43:16 +0300

    Initial Commit

---
+++ CustomNavigationSwiftUI/CustomNavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationSwiftUIApp.swift
+//  CustomNavigationSwiftUI
+//
+//  Created by Даниил Дорожкин on 16/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
